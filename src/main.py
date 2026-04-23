bae4c4cb19b449ae61a772a9f97a8f6eaf340072
nh916 <navidhariri25@gmail.com>
2026-04-23 11:42:55 -0700

    added `main.py` to the project for setup

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Entry point for the application.
+"""
+
+
+def main() -> None:
+    """
+    Entry point for the application.
+    Currently, just prints a message to the console.
+    """
+
+    print("Hello, World!")
